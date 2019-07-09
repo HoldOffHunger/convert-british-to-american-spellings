c6b1a3a8953d09be6be0233f099d33d28a55ad64
HoldOffHunger <holdoffhunger@gmail.com>
2019-07-09 19:42:33 -0400

    Create AmericanBritishWords_Z.php

---
+++ classes/Language/Words/AmericanBritish/AmericanBritishWords_Z.php
@@ -0,0 +1,39 @@
+<?php
+
+	class AmericanBritishWords_Z {
+		public function __construct($args) {
+			return TRUE;
+		}
+
+		public function AmericanBritishWords() {
+			return [
+				'zaffer'=>'zaffre',
+				'zaffers'=>'zaffres',
+				'zea'=>'zoea',
+				'zeas'=>'zoeas',
+				'zeroize'=>'zeroise',
+				'zeroized'=>'zeroised',
+				'zeroizes'=>'zeroises',
+				'zeroizing'=>'zeroising',
+				'zoea\'s'=>'zoaea\'s',
+				'zoea'=>['zoaea', 'zooea',],
+				'zoeae'=>'zooeae',
+				'zoeal'=>'zooeal',
+				'zoeas'=>'zooeas',
+				'zoecia'=>'zooecia',
+				'zoecium'=>'zooecium',
+				'zombie\'s'=>'zombi\'s',
+				'zombie'=>'zombi',
+				'zombies'=>'zombis',
+				'zonesthesia'=>'zonaesthesia',
+				'zonule'=>'zonulae',
+				'zooglea'=>'zoogloea',
+				'zoogleae'=>'zoogloeae',
+				'zoogleal'=>'zoogloeal',
+				'zoogleas'=>'zoogloeas',
+				'zygenid'=>'zygaenid',
+			];
+		}
+	}
+	
+?>
