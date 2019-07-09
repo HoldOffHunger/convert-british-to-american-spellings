f27b8f35adc36664ca80f79c4e8107a9c00fec0d
HoldOffHunger <holdoffhunger@gmail.com>
2019-07-09 19:42:22 -0400

    Create AmericanBritishWords_Y.php

---
+++ classes/Language/Words/AmericanBritish/AmericanBritishWords_Y.php
@@ -0,0 +1,36 @@
+<?php
+
+	class AmericanBritishWords_Y {
+		public function __construct($args) {
+			return TRUE;
+		}
+
+		public function AmericanBritishWords() {
+			return [
+				'yak\'s'=>'yack\'s',
+				'yak'=>'yack',
+				'yakked'=>'yacked',
+				'yakking'=>'yacking',
+				'yaks'=>'yacks',
+				'yellow-gray'=>'yellow-grey',
+				'yeshiva\'s'=>'yeshivah\'s',
+				'yeshiva'=>'yeshivah',
+				'yeshivas'=>['yeshivot', 'yeshivahs',],
+				'yodeled'=>'yodelled',
+				'yodeler\'s'=>'yodeller\'s',
+				'yodeler'=>'yodeller',
+				'yodelers'=>'yodellers',
+				'yodeling'=>'yodelling',
+				'yogi\'s'=>'yogin\'s',
+				'yogi'=>'yogin',
+				'yogis'=>'yogins',
+				'yogurt\'s'=>['yoghurt\'s', 'yoghourt\'s', 'yogourt\'s',],
+				'yogurt'=>['yoghurt', 'yoghourt', 'yogourt',],
+				'yogurts'=>['yoghurts', 'yoghourts', 'yogourts',],
+				'yuck'=>'yuk',
+				'yucky'=>['yuky', 'yukky',],
+			];
+		}
+	}
+	
+?>
