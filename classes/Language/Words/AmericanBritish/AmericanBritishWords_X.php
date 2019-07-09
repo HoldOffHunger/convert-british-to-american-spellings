851068f119d79482462a29e0ed76a29ba5e10e72
HoldOffHunger <holdoffhunger@gmail.com>
2019-07-09 19:42:04 -0400

    Create AmericanBritishWords_X.php

---
+++ classes/Language/Words/AmericanBritish/AmericanBritishWords_X.php
@@ -0,0 +1,14 @@
+<?php
+
+	class AmericanBritishWords_X {
+		public function __construct($args) {
+			return TRUE;
+		}
+
+		public function AmericanBritishWords() {
+			return [
+			];
+		}
+	}
+	
+?>
