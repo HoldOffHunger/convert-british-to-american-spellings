218430df8f71362e6ac7a8cee1fa59b29174f6ab
HoldOffHunger <holdoffhunger@gmail.com>
2019-08-13 21:41:34 -0400

    Update AmericanBritishWords_V.php

--- classes/Language/Words/AmericanBritish/AmericanBritishWords_V.php
+++ classes/Language/Words/AmericanBritish/AmericanBritishWords_V.php
@@ -1,9 +1,29 @@
 <?php
+				
+				/* AmericanBritishWords_V
+					
+					Class for American/British spelling variants for all words beginning with : V.
+					
+				*/
 
 	class AmericanBritishWords_V {
+			/* __construct($args)
+			
+				Constructor.
+				
+				Nothing to do here.
+			
+			*/
+			
 		public function __construct($args) {
 			return TRUE;
 		}
+		
+			/* AmericanBritishWords()
+			
+				List of US/UK spellings for words starting with : V.
+			
+			*/
 
 		public function AmericanBritishWords() {
 			return [
