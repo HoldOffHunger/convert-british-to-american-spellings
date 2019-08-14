2d0d99b0c14c51b93033e169afc01e6157993cf2
HoldOffHunger <holdoffhunger@gmail.com>
2019-08-13 21:42:44 -0400

    Update AmericanBritishWords_Z.php

--- classes/Language/Words/AmericanBritish/AmericanBritishWords_Z.php
+++ classes/Language/Words/AmericanBritish/AmericanBritishWords_Z.php
@@ -1,9 +1,29 @@
 <?php
+				
+				/* AmericanBritishWords_Z
+					
+					Class for American/British spelling variants for all words beginning with : Z.
+					
+				*/
 
 	class AmericanBritishWords_Z {
+			/* __construct($args)
+			
+				Constructor.
+				
+				Nothing to do here.
+			
+			*/
+			
 		public function __construct($args) {
 			return TRUE;
 		}
+		
+			/* AmericanBritishWords()
+			
+				List of US/UK spellings for words starting with : Z.
+			
+			*/
 
 		public function AmericanBritishWords() {
 			return [
