ef99eb8233edd632f820432b06f074c5317b052c
HoldOffHunger <holdoffhunger@gmail.com>
2019-08-13 21:39:06 -0400

    Update AmericanBritishWords_M.php

--- classes/Language/Words/AmericanBritish/AmericanBritishWords_M.php
+++ classes/Language/Words/AmericanBritish/AmericanBritishWords_M.php
@@ -1,9 +1,29 @@
 <?php
+				
+				/* AmericanBritishWords_M
+					
+					Class for American/British spelling variants for all words beginning with : M.
+					
+				*/
 
 	class AmericanBritishWords_M {
+			/* __construct($args)
+			
+				Constructor.
+				
+				Nothing to do here.
+			
+			*/
+			
 		public function __construct($args) {
 			return TRUE;
 		}
+		
+			/* AmericanBritishWords()
+			
+				List of US/UK spellings for words starting with : M.
+			
+			*/
 
 		public function AmericanBritishWords() {
 			return [
