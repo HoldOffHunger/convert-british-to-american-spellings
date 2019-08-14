f834b89623b480bd2ae54e67cbbdbbb3b757157b
HoldOffHunger <holdoffhunger@gmail.com>
2019-08-13 21:25:23 -0400

    Update AmericanBritishWords_A.php

--- classes/Language/Words/AmericanBritish/AmericanBritishWords_A.php
+++ classes/Language/Words/AmericanBritish/AmericanBritishWords_A.php
@@ -1,9 +1,29 @@
 <?php
+				
+				/* AmericanBritishWords_A
+					
+					Class for American/British spelling variants for all words beginning with : A.
+					
+				*/
 
 	class AmericanBritishWords_A {
+			/* __construct($args)
+			
+				Constructor.
+				
+				Nothing to do here.
+			
+			*/
+			
 		public function __construct($args) {
 			return TRUE;
 		}
+		
+			/* AmericanBritishWords()
+			
+				List of US/UK spellings for words starting with : A.
+			
+			*/
 
 		public function AmericanBritishWords() {
 			return [
