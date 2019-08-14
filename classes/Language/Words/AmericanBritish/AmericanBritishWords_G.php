8801b2a7a316eca807fa4f9058a6be9416cbcd18
HoldOffHunger <holdoffhunger@gmail.com>
2019-08-13 21:37:17 -0400

    Update AmericanBritishWords_G.php

--- classes/Language/Words/AmericanBritish/AmericanBritishWords_G.php
+++ classes/Language/Words/AmericanBritish/AmericanBritishWords_G.php
@@ -1,9 +1,29 @@
 <?php
+				
+				/* AmericanBritishWords_G
+					
+					Class for American/British spelling variants for all words beginning with : G.
+					
+				*/
 
 	class AmericanBritishWords_G {
+			/* __construct($args)
+			
+				Constructor.
+				
+				Nothing to do here.
+			
+			*/
+			
 		public function __construct($args) {
 			return TRUE;
 		}
+		
+			/* AmericanBritishWords()
+			
+				List of US/UK spellings for words starting with : G.
+			
+			*/
 
 		public function AmericanBritishWords() {
 			return [
